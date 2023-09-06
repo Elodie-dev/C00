eac108f944db71f65c8c6d17009c0b01d9a66c31
Elodie De Cola <ede-cola@paul-f3Br5s8.clusters.42paris.fr>
2023-09-06 20:09:41 +0200

    ex02

---
+++ ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ede-cola <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/06 20:06:01 by ede-cola          #+#    #+#             */
+/*   Updated: 2023/09/06 20:09:17 by ede-cola         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	lettre;
+
+	lettre = 'z';
+	while (lettre >= 'a')
+	{
+		write(1, &lettre, 1);
+		lettre--;
+	}	
+}
